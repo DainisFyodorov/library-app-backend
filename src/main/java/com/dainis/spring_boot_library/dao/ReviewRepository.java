4178d2580f78c91ec3400fa0f60892974241bf47
DainisFyodorov <fedorovdainis@gmail.com>
2025-11-06 23:37:57 +0200

    Find review by book id implemented

--- src/main/java/com/dainis/spring_boot_library/dao/ReviewRepository.java
+++ src/main/java/com/dainis/spring_boot_library/dao/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.dainis.spring_boot_library.dao;
 
 import com.dainis.spring_boot_library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("bookId") Long bookId, Pageable pageable);
 }
