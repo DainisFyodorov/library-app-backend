7947eb13101c8dd0c1f5d8c09a90721f47985915
DainisFyodorov <fedorovdainis@gmail.com>
2025-11-25 00:53:02 +0200

    JWT Extraction implemented

--- src/main/java/com/dainis/spring_boot_library/config/SecurityConfiguration.java
+++ src/main/java/com/dainis/spring_boot_library/config/SecurityConfiguration.java
@@ -3,12 +3,13 @@ package com.dainis.spring_boot_library.config;
 import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
@@ -16,29 +17,25 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // Disable Cross Site Request Forgery
+        // Protect endpoints at /api/<type>/secure
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/books/secure/**")
+                .authenticated()
+                .anyRequest().permitAll()
+        );
+
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+
+        http.cors(withDefaults());
+
         http.csrf(AbstractHttpConfigurer::disable);
 
-        // Protect endpoints at /api/<type>/secure
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers("/api/books/secure/**")
-                        .authenticated())
-                .oauth2ResourceServer(
-                        (oauth2) ->
-                                oauth2.jwt(Customizer.withDefaults())
-                );
-
-        // Add CORS filters
-        http.cors(Customizer.withDefaults());
-
-        // Add content negotiation strategy
         http.setSharedObject(ContentNegotiationStrategy.class,
                 new HeaderContentNegotiationStrategy());
 
-        // Force a non-empty response body for 401's to make the response friendly
         Okta.configureResourceServer401ResponseBody(http);
 
         return http.build();
     }
+
 }
--- src/main/java/com/dainis/spring_boot_library/controller/BookController.java
+++ src/main/java/com/dainis/spring_boot_library/controller/BookController.java
@@ -2,7 +2,10 @@ package com.dainis.spring_boot_library.controller;
 
 import com.dainis.spring_boot_library.entity.Book;
 import com.dainis.spring_boot_library.service.BookService;
+import com.dainis.spring_boot_library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
@@ -18,20 +21,22 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"email\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@AuthenticationPrincipal Jwt jwt,
+                                      @RequestParam Long bookId) {
+        String userEmail = jwt.getClaim("email");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@AuthenticationPrincipal Jwt jwt,
+                             @RequestParam Long bookId) throws Exception {
+        String userEmail = jwt.getClaim("email");
         return bookService.checkoutBook(userEmail, bookId);
     }
 }
