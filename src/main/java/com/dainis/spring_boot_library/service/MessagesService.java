ec1781a3f895aa4b25016ee8fe5d802b927e8dc4
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-26 00:14:52 +0200

    Messages Service implemented

---
+++ src/main/java/com/dainis/spring_boot_library/service/MessagesService.java
@@ -0,0 +1,26 @@
+package com.dainis.spring_boot_library.service;
+
+import com.dainis.spring_boot_library.dao.MessageRepository;
+import com.dainis.spring_boot_library.entity.Message;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessagesService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage(Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+
+}
