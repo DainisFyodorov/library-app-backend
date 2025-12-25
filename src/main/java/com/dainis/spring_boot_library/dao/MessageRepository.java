e04e37666398cd1da554a093d9741ede02e0256a
DainisFyodorov <fedorovdainis@gmail.com>
2025-12-26 00:11:43 +0200

    Message Repository implemented

---
+++ src/main/java/com/dainis/spring_boot_library/dao/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.dainis.spring_boot_library.dao;
+
+import com.dainis.spring_boot_library.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
