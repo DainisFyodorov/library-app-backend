0eca2a3cec801e7adbd115a505b742d851a093a8
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-14 19:30:48 +0200

    PUT mapping for "/secure/admin/message" endpoint implemented

--- src/main/java/com/dainis/spring_boot_library/controller/MessagesController.java
+++ src/main/java/com/dainis/spring_boot_library/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package com.dainis.spring_boot_library.controller;
 
 import com.dainis.spring_boot_library.entity.Message;
+import com.dainis.spring_boot_library.requestmodels.AdminQuestionRequest;
 import com.dainis.spring_boot_library.service.MessagesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,5 +25,18 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = jwt.getClaim("email");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@AuthenticationPrincipal Jwt jwt,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = jwt.getClaim("email");
+        String userType = jwt.getClaim("userType");
+
+        if(userType == null || !userType.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
