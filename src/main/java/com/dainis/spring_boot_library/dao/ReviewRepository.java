6b7177fe76fd4d01d8095938006b3f74e89b30aa
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-20 17:45:20 +0200

    Checkout and Review repository's sql query fixed

--- src/main/java/com/dainis/spring_boot_library/dao/ReviewRepository.java
+++ src/main/java/com/dainis/spring_boot_library/dao/ReviewRepository.java
@@ -16,6 +16,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findByUserEmailAndBookId(String userEmail, Long bookId);
 
     @Modifying
-    @Query("delete from Review where book_id in :book_id")
+    @Query("delete from Review r where r.bookId in :book_id")
     void deleteAllByBookId(@Param("book_id") Long bookId);
 }
