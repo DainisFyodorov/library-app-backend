d69a1644b504062f286d0625088ef09e22852098
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-20 16:29:30 +0200

    Increase book quantity endpoint in AdminController implemented

--- src/main/java/com/dainis/spring_boot_library/controller/AdminController.java
+++ src/main/java/com/dainis/spring_boot_library/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.dainis.spring_boot_library.controller;
 
 import com.dainis.spring_boot_library.requestmodels.AddBookRequest;
 import com.dainis.spring_boot_library.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,18 @@ public class AdminController {
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@AuthenticationPrincipal Jwt jwt,
+                                     @RequestParam Long bookId) throws Exception {
+        String userType = jwt.getClaim("userType");
+
+        if(userType == null || !userType.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+
+        adminService.increaseBookQuantity(bookId);
     }
 
     @PostMapping("/secure/add/book")
