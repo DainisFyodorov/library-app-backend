be9b2c07a2f1c07b06b9ed2df3f87972d2efc3e9
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-24 00:21:40 +0200

    PaymentService implemented

---
+++ src/main/java/com/dainis/spring_boot_library/service/PaymentService.java
@@ -0,0 +1,22 @@
+package com.dainis.spring_boot_library.service;
+
+import com.dainis.spring_boot_library.dao.PaymentRepository;
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+
+        Stripe.apiKey = secretKey;
+    }
+}
