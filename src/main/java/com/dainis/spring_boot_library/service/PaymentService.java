2b7f7526b122c37cfd3ecbd71f0b045241725d66
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-24 00:25:11 +0200

    createPaymentIntent method in PaymentService implemented

--- src/main/java/com/dainis/spring_boot_library/service/PaymentService.java
+++ src/main/java/com/dainis/spring_boot_library/service/PaymentService.java
@@ -1,11 +1,19 @@
 package com.dainis.spring_boot_library.service;
 
 import com.dainis.spring_boot_library.dao.PaymentRepository;
+import com.dainis.spring_boot_library.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -18,5 +26,17 @@ public class PaymentService {
         this.paymentRepository = paymentRepository;
 
         Stripe.apiKey = secretKey;
+    }
+
+    public PaymentIntent createPaymentIntent(PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("currency", paymentInfoRequest.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 }
