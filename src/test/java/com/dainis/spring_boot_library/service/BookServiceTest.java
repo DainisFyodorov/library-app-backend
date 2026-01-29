1bb77e289c47e7eecf1a4bc73a4d62750d2abe6c
DainisFyodorov <fedorovdainis@gmail.com>
2026-01-29 10:06:27 +0200

    BookService testCheckoutBookSuccess unit test implemented

---
+++ src/test/java/com/dainis/spring_boot_library/service/BookServiceTest.java
@@ -0,0 +1,69 @@
+package com.dainis.spring_boot_library.service;
+
+import com.dainis.spring_boot_library.dao.BookRepository;
+import com.dainis.spring_boot_library.dao.CheckoutRepository;
+import com.dainis.spring_boot_library.dao.HistoryRepository;
+import com.dainis.spring_boot_library.dao.PaymentRepository;
+import com.dainis.spring_boot_library.entity.Book;
+import com.dainis.spring_boot_library.entity.Payment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private CheckoutRepository checkoutRepository;
+
+    @Mock
+    private HistoryRepository historyRepository;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    @DisplayName("Successfully checkout the book without fees and book is available")
+    void testCheckoutBookSuccess() throws Exception {
+        String userEmail = "test@example.com";
+        Long bookId = 1L;
+
+        Book book = new Book();
+        book.setId(bookId);
+        book.setCopiesAvailable(10);
+        book.setTitle("Test book");
+
+        Payment payment = new Payment();
+        payment.setUserEmail(userEmail);
+        payment.setAmount(0.00);
+
+        when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        when(checkoutRepository.findByUserEmailAndBookId(userEmail, bookId)).thenReturn(null);
+        when(checkoutRepository.findBooksByUserEmail(userEmail)).thenReturn(Collections.emptyList());
+        when(paymentRepository.findByUserEmail(userEmail)).thenReturn(payment);
+
+        Book result = bookService.checkoutBook(userEmail, bookId);
+
+        assertNotNull(result);
+        assertEquals(9, result.getCopiesAvailable(), "Amount of books available should be decreased by 1, equals 9");
+
+        verify(bookRepository, times(1)).save(any(Book.class));
+        verify(checkoutRepository, times(1)).save(any());
+    }
+}
