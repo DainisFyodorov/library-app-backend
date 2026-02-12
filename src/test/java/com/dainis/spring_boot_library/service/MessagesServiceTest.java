686ffd08d3ff9b1302dc7742765cd2b44b1ff153
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-12 15:51:58 +0200

    MessagesService testPutMessageSuccess unit test implemented

--- src/test/java/com/dainis/spring_boot_library/service/MessagesServiceTest.java
+++ src/test/java/com/dainis/spring_boot_library/service/MessagesServiceTest.java
@@ -2,16 +2,18 @@ package com.dainis.spring_boot_library.service;
 
 import com.dainis.spring_boot_library.dao.MessageRepository;
 import com.dainis.spring_boot_library.entity.Message;
+import com.dainis.spring_boot_library.requestmodels.AdminQuestionRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MessagesServiceTest {
@@ -25,12 +27,33 @@ public class MessagesServiceTest {
     @Test
     void testPostMessageSuccess() {
         String userEmail = "test@example.com";
-
         Message messageRequest = new Message("Test title", "Test question");
-        messageRequest.setUserEmail(userEmail);
 
         messagesService.postMessage(messageRequest, userEmail);
 
         verify(messageRepository, times(1)).save(any(Message.class));
+    }
+
+    @Test
+    void testPutMessageSuccess() throws Exception {
+        String adminEmail = "admin@example.com";
+        Long messageId = 1L;
+
+        Message existingMessage = new Message("Test title", "Test question");
+        existingMessage.setId(messageId);
+        existingMessage.setClosed(false);
+
+        AdminQuestionRequest adminQuestionRequest = new AdminQuestionRequest();
+        adminQuestionRequest.setId(messageId);
+        adminQuestionRequest.setResponse("Test response");
+
+        when(messageRepository.findById(existingMessage.getId())).thenReturn(Optional.of(existingMessage));
+
+        assertDoesNotThrow(() -> messagesService.putMessage(adminQuestionRequest, adminEmail));
+        assertEquals(adminQuestionRequest.getResponse(), existingMessage.getResponse());
+        assertEquals(adminEmail, existingMessage.getAdminEmail());
+        assertTrue(existingMessage.isClosed());
+
+        verify(messageRepository, times(1)).save(existingMessage);
     }
 }
